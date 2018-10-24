b2b05e771bb3f83a88ade84fffbc2cee422b9239
ShantonWaldien <waldienshanton@gmail.com>
2018-10-24 11:44:39 +0200

    created a paragraph using variables and displayed it in console log

--- js/main.js
+++ js/main.js
@@ -1,3 +1,27 @@
 // this is an alert
 
 // alert("I am learning JavaScript!");
+
+// var message = 'Good Morning';
+//     message = 'How are you today?';
+// alert(message);
+
+// var user = 'Shanton';
+// alert(user);
+
+// var age = '20';
+// alert(age);
+
+// var gender = 'Male';
+// alert(gender);
+
+// declare two variables student and admin  assign the value "my name" then copy the value of student to admin then show the value of admin using alert. the output should be your name.
+
+// var student = 'Shanton Waldien';
+//     admin = student ;
+// alert(admin);
+
+var greetings = 'Good Morning, ';
+    user = ' Shanton Waldien. ' ;
+    address = ' 36 Glenside street.';
+console.log(greetings + "My name is" + user + "This is my address" + address);
